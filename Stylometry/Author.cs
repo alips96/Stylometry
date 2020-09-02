7cfa616296daa8f7e3164900971b5bed7bf436a6
Ali <alips96@gmail.com>
2020-09-02 22:01:49 +0200

    author records saved and classified

---
+++ Stylometry/Author.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Stylometry
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Topic { get; set; }
+        public string Sign { get; set; }
+        public string Date { get; set; }
+        public string Text { get; set; }
+
+        internal static Author ParseRow(string row)
+        {
+            string[] column = TrimRow(ref row);
+
+            return new Author()
+            {
+                Id = Convert.ToInt32(column[0]),
+                Gender = column[1],
+                Age = Convert.ToInt32(column[2]),
+                Topic = column[3],
+                Sign = column[4],
+                Date = GetBlogDate(column),
+                Text = GetAuthorText(column)
+            };
+        }
+
+        private static string[] TrimRow(ref string row)
+        {
+            row = row.Replace("\"\"", "");
+            row = row.Trim(new char[] { '"' });
+            string[] column = row.Split(',');
+            return column;
+        }
+
+        private static string GetBlogDate(string[] column)
+        {
+            string date = column[5] + "/" + column[6] + "/" + column[7];
+            return date;
+        }
+
+        private static string GetAuthorText(string[] column)
+        {
+            string text = null;
+
+            for (int i = 8; i < column.Length; i++)
+            {
+                text += column[i];
+            }
+
+            text = text.Trim(); //remove emty spaces
+            return text;
+        }
+    }
+}
--- Stylometry/Form1.cs
+++ Stylometry/Form1.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Stylometry
@@ -15,6 +12,21 @@ namespace Stylometry
         public Form1()
         {
             InitializeComponent();
+            CreateAuthorsList();
+        }
+
+        private void CreateAuthorsList()
+        {
+            List<Author> authors = ProcessCorpus("Corpus.csv");
+            StartButton.Text = authors[0].Text;
+        }
+
+        private List<Author> ProcessCorpus(string path)
+        {
+            return File.ReadAllLines(path).
+                Skip(1).
+                Where(row => row.Length > 0).
+                Select(Author.ParseRow).ToList();
         }
 
         private void StartButton_Clicked(object sender, EventArgs e)
