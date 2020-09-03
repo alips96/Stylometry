0c3975a81ba319ede3595ba6fc020c802d9d8d9f
Ali <alips96@gmail.com>
2020-09-03 13:11:51 +0200

    Feauture class initial setup

---
+++ Stylometry/Feature.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stylometry
+{
+    public class Feature
+    {
+        public int Sentence { get; set; }
+        public int WordCount { get; set; }
+        public int AverageLetterCount { get; set; }
+        public int NounCount { get; set; }
+        public int VerbCount { get; set; }
+        public int MostCommonWordCount { get; set; }
+        public int SecondMostCommonWordCount { get; set; }
+        public int AuthorId { get; set; }
+
+        public List<string> TrainTokens { get; set; }
+        public List<string> TestTokens { get; set; }
+
+        public Feature(int _Sentence, int _WordCound, int _AverageLetterCount
+            , int _NounCount, int _VerbCount, int _MostCommonWordCount,
+            int _SecondMostCommonWordCount, int _AuthorId)
+        {
+            Sentence = _Sentence;
+            WordCount = _WordCound;
+            AverageLetterCount = _AverageLetterCount;
+            NounCount = _NounCount;
+            VerbCount = _VerbCount;
+            MostCommonWordCount = _MostCommonWordCount;
+            SecondMostCommonWordCount = _SecondMostCommonWordCount;
+            AuthorId = _AuthorId;
+        }
+    }
+}
